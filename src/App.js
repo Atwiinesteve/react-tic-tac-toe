2fbca07c72e760d1ada42053ebbb5c7722c9a6f3
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-01-20 10:18:24 +0000

    TicTacToe game setup ready

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import react
+import React from 'react';
+
+// import styles
+
+// import components
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div className="game--container">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Board.jsx
@@ -0,0 +1,17 @@
+// import react
+import React from 'react';
+
+// import styles
+import "./Board";
+
+// Board Component
+function Board() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+// export board
+export default Board;
